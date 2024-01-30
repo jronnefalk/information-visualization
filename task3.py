dcebfbb5ce4043f5581618989ffb00399ea59f24
jronnefalk <julia@ronnefalk.se>
2024-01-30 11:03:20 +0100

    fix

--- task3.py
+++ task3.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 import csv
 
+file_path = "data1.csv"
+
+
 def read_csv(file_path):
     x_values = []
     y_values = []
     categories = []
 
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         reader = csv.reader(file)
         for row in reader:
             x_values.append(float(row[0]))
@@ -14,3 +17,18 @@ def read_csv(file_path):
             categories.append(row[2])
 
     return x_values, y_values, categories
+
+
+x_values, y_values, categories = read_csv(file_path)
+
+# Find the minimum and maximum values
+xmin = min(x_values)
+xmax = max(x_values)
+
+ymin = min(y_values)
+ymax = max(y_values)
+
+print(f"Minimum value of x_values: ", xmin)
+print(f"Maximum value of x_values: ", xmax)
+print(f"Minimum value of y_values: ", ymin)
+print(f"Maximum value of y_values: ", ymax)
